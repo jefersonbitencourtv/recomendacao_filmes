09cf4d6b74c39f2376802ae5af47121594a32ff9
Jeferson Bitencourt <jeferson267@hotmail.com>
2022-07-07 15:22:23 -0300

    Start project

---
+++ src/main/java/api/desafio/recomendacao_filmes/RecomendacaoFilmesApplication.java
@@ -0,0 +1,13 @@
+package api.desafio.recomendacao_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendacaoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendacaoFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/desafio/recomendacao_filmes/RecomendacaoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.desafio.recomendacao_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendacaoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
